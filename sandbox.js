3500e3e92d46450f6553c3e061500df0c3db3e80
nandar-dev <mgnandar290@gmail.com>
2023-03-05 10:53:41 +0630

    init

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var char = "some";
+var age = 30;
+var isMale = true;
+char = "20";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ("dksl"));
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+let char = "some";
+let age = 30;
+let isMale = true;
+
+char = "20";
+
+
+const circ = (diameter) =>{
+    return diameter * Math.PI;
+}
+
+console.log(circ("dksl"));
+

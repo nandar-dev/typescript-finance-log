f919ce1736ac9e636da704760bce0db8f1e38300
nandar-dev <mgnandar290@gmail.com>
2023-03-05 23:05:18 +0630

    update

--- public/app.js
+++ public/app.js
@@ -1,4 +1,36 @@
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+// const me: IsPerson = {
+//   name: "shaun",
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log("I spend ", amount);
+//     return amount;
+//   },
+// };
+// const greetPerson = (person: IsPerson) =>{
+//   console.log('hello', person.name);
+// }
+// greetPerson(me)
+// console.log(me);
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplates.js";
+import { Payment } from "./classes/Payment.js";
+let docOne;
+let docTwo;
+docOne = new Invoice("yoshi", 'web work', 250);
+docTwo = new Payment("mario", 'plumbing work', 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice("mario", "work on the web", 250);
 const invTwo = new Invoice("luigi", "work on the web", 300);
 let invoices = [];
@@ -7,14 +39,41 @@ invoices.push(invTwo);
 invoices.forEach((inv) => {
     console.log(inv.client, inv.amount, inv.format());
 });
-console.log("mmm", invoices);
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOnee = addUID({ name: "yoshi", age: 20 });
+console.log(docOnee.age);
+const docThreee = {
+    uid: 1,
+    reourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 2,
+    reourceName: "dkdkd",
+    data: ['a', 'b', 'c']
+};
+console.log(docThreee, docFour);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,49 @@
+
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+
+// const me: IsPerson = {
+//   name: "shaun",
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log("I spend ", amount);
+//     return amount;
+//   },
+// };
+
+// const greetPerson = (person: IsPerson) =>{
+//   console.log('hello', person.name);
+  
+// }
+
+// greetPerson(me)
+
+// console.log(me);
+
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplates.js";
+import { Payment } from "./classes/Payment.js";
+import {HasFormatter} from "./interfaces/HasFormatter.js"
+
+let docOne : HasFormatter;
+let docTwo : HasFormatter;
+
+docOne = new Invoice("yoshi", 'web work', 250);
+docTwo = new Payment("mario", 'plumbing work', 200);
+
+let docs : HasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
+
+console.log(docs);
+
 
 const invOne = new Invoice("mario", "work on the web", 250);
 const invTwo = new Invoice("luigi", "work on the web", 300);
@@ -11,8 +56,6 @@ invoices.forEach((inv) => {
   console.log(inv.client, inv.amount, inv.format());
 });
 
-console.log("mmm", invoices);
-
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -23,8 +66,54 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value);
+  let doc : HasFormatter;
+
+  if(type.value === 'invoice'){
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+  }else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+  }
+
+  list.render(doc,type.value,'end')
 });
+
+// GENERICS
+
+const addUID = <T extends object>(obj: T)=>{
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj,uid};
+}
+
+let docOnee = addUID({name: "yoshi", age: 20});
+
+console.log(docOnee.age);
+
+// with interface
+interface Resource<T> {
+  uid: number,
+  reourceName: string, 
+  data: T
+}
+
+const docThreee: Resource<object> = {
+uid: 1,
+reourceName: 'person',
+data:  {name: 'shaun'}
+}
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  reourceName: "dkdkd",
+  data: ['a','b','c']
+}
+
+console.log(docThreee,docFour);
